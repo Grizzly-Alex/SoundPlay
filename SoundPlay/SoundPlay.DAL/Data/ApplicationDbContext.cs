00c8f1b5d7d97b3755a45fa725196fcbcf5ba963
Alexander Medved <medved1991alexander@gmail.com>
2023-01-06 20:43:12 +0300

    Add Dependencies

---
+++ SoundPlay/SoundPlay.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SoundPlay.DAL.Data
+{
+	public sealed class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+		}
+	}
+}

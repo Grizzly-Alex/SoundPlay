d103d480e660c054e338a37e49d63cc850c6b02b
anton <antonprokapnev@gmail.com>
2023-01-09 19:42:20 +0300

    Add CategoryService

---
+++ SoundPlay/SoundPlay.BLL/Services/CategoryService.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
+using SoundPlay.BLL.Exceptions;
+using SoundPlay.BLL.Interfaces;
+using SoundPlay.DAL.Repository.Interfaces;
+using SoundPlay.DTO.Models;
+using SoundPlay.DTO.ViewModels;
+
+namespace SoundPlay.BLL.Services
+{
+    public class CategoryService:IGenericService<CategoryViewModel>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CategoryService> _logger;
+
+        public CategoryService(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CategoryService> logger)
+        {
+            _unitOfWork=unitOfWork;
+            _mapper=mapper;
+            _logger=logger;
+        }
+
+        public async Task<CategoryViewModel> CreateViewModelAsync(CategoryViewModel viewModel)
+        {
+            var model = _mapper.Map<Category>(viewModel);
+
+            try
+            {
+                _unitOfWork.Category.Add(model);
+                await _unitOfWork.SaveChangesAsync();
+                _logger.LogInformation("Create operation is successfull");
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Create operation is failed, {ex.Message}");
+            }
+
+            return viewModel;
+        }
+
+        public async Task<CategoryViewModel> DeleteViewModelAsync(CategoryViewModel viewModel)
+        {
+            var model = _mapper.Map<Category>(viewModel);
+
+            try
+            {
+                _unitOfWork.Category.Remove(model);
+                await _unitOfWork.SaveChangesAsync();
+                _logger.LogInformation("Delete operation is successfull");
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogInformation($"Delete operation is failed, {ex.Message}");
+            }
+
+            return viewModel;
+        }
+
+        public async Task<CategoryViewModel> GetViewModelByIdAsync(int id)
+        {
+            var model = await _unitOfWork.Category.GetFirstOrDefaultAsync(b => b.Id==id);
+
+            if (model==null)
+            {
+                _logger.LogError("Get_by_id operation is failed");
+                throw new ObjectNotFoundException("No brand found");
+            }
+
+            _logger.LogInformation("Get_by_id operation is successfull");
+            var viewModel = _mapper.Map<CategoryViewModel>(model);
+            return viewModel;
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetViewModelsAsync()
+        {
+            var models = await _unitOfWork.Category.GetAllAsync();
+
+            if (models.Count()==0||models==null)
+            {
+                _logger.LogError("Get_All operation is failed");
+                throw new ObjectNotFoundException("No brand found");
+            }
+
+            _logger.LogInformation("Get_All operation is successfull");
+            var viewModels = _mapper.Map<IEnumerable<CategoryViewModel>>(models);
+            return viewModels;
+        }
+
+        public async Task<CategoryViewModel> UpdateViewModelAsync(CategoryViewModel viewModel)
+        {
+            var model = _mapper.Map<Category>(viewModel);
+
+            try
+            {
+                _unitOfWork.Category.Update(model);
+                await _unitOfWork.SaveChangesAsync();
+                _logger.LogInformation("Update operation is successfull");
+            }
+
+            catch (Exception ex)
+            {
+                _logger.LogError($"Update operation is failed, {ex.Message}");
+            }
+
+            return viewModel;
+        }
+    }
+}

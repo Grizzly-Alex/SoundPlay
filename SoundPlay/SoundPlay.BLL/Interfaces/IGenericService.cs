cfd6a4d6511695b5c1d5fbb034e54aa1c582f1bc
anton <antonprokapnev@gmail.com>
2023-01-09 18:24:49 +0300

    Add IGenericService interface

---
+++ SoundPlay/SoundPlay.BLL/Interfaces/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿namespace SoundPlay.BLL.Interfaces
+{
+    public interface IGenericService<TViewModel> where TViewModel : class
+    {
+        public Task<IEnumerable<TViewModel>> GetMeetupsAsync();
+
+        public Task<TViewModel> GetMeetupByIdAsync(int id);
+
+        public Task<TViewModel> CreateMeetupAsync(TViewModel viewModel);
+
+        public Task<TViewModel> UpdateMeetupAsync(TViewModel viewModel);
+
+        public Task<TViewModel> DeleteMeetupAsync(TViewModel viewModel);
+    }
+}

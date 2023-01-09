e224b12dd074c66c7b66ac9bfd3856bb21b973e2
anton <antonprokapnev@gmail.com>
2023-01-09 21:21:02 +0300

    Add Postgre connection

--- SoundPlay/SoundPlay.DAL/Dependencies.cs
+++ SoundPlay/SoundPlay.DAL/Dependencies.cs
@@ -11,10 +11,13 @@ namespace SoundPlay.DAL
 	{
 		public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlServer(configuration.GetConnectionString("CatalogConnection")));
+			//services.AddDbContext<ApplicationDbContext>(options =>
+			//	options.UseSqlServer(configuration.GetConnectionString("CatalogConnection")));
 
-			services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
 		}
 	}
 }

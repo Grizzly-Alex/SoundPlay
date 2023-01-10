8099a2bbe748b764601855e0444ee3f059541022
Alexander Medved <medved1991alexander@gmail.com>
2023-01-10 23:28:57 +0300

    add tremoloType controller

---
+++ SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/TremoloTypeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundPlay.BLL.Interfaces;
+using SoundPlay.BLL.ViewModels;
+
+namespace SoundPlay.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TremoloTypeController : Controller
+    {
+        private readonly IItemGenericService<TremoloTypeViewModel> _tremoloTypeService;
+
+        public TremoloTypeController(IItemGenericService<TremoloTypeViewModel> tremoloTypeService)
+        {
+            _tremoloTypeService = tremoloTypeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var viewModels = await _tremoloTypeService.GetViewModelsAsync();
+            return View(viewModels);
+        }
+        
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TremoloTypeViewModel obj)
+        {    
+            if (ModelState.IsValid)
+            {
+				await _tremoloTypeService.CreateViewModelAsync(obj);
+				return RedirectToAction("Index");
+			}
+            return View(obj);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+			var viewModel = await _tremoloTypeService.GetViewModelByIdAsync(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(TremoloTypeViewModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+				await _tremoloTypeService.UpdateViewModelAsync(obj);
+				return RedirectToAction("Index");
+			}
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var viewModel = await _tremoloTypeService.GetViewModelByIdAsync(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(TremoloTypeViewModel obj)
+        {
+			await _tremoloTypeService.DeleteViewModelAsync(obj);
+			return RedirectToAction("Index");
+		}
+    }
+}

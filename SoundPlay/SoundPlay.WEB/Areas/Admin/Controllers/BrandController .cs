fe66cca5b8e7ffe895214306fda980a400917b48
Alexander Medved <medved1991alexander@gmail.com>
2023-01-10 23:13:15 +0300

    add brand controller

---
+++ SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/BrandController .cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoundPlay.BLL.Interfaces;
+using SoundPlay.BLL.ViewModels;
+
+namespace SoundPlay.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly IItemGenericService<BrandViewModel> _brandService;
+
+        public BrandController(IItemGenericService<BrandViewModel> brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var viewModels = await _brandService.GetViewModelsAsync();
+            return View(viewModels);
+        }
+        
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(BrandViewModel obj)
+        {    
+            if (ModelState.IsValid)
+            {
+				await _brandService.CreateViewModelAsync(obj);
+				return RedirectToAction("Index");
+			}
+            return View(obj);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+			var viewModel = await _brandService.GetViewModelByIdAsync(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BrandViewModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+				await _brandService.UpdateViewModelAsync(obj);
+				return RedirectToAction("Index");
+			}
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var viewModel = await _brandService.GetViewModelByIdAsync(id);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(BrandViewModel obj)
+        {
+			await _brandService.DeleteViewModelAsync(obj);
+			return RedirectToAction("Index");
+		}
+    }
+}

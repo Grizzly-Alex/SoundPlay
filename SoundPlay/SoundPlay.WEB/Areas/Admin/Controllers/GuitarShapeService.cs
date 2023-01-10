de5a7a6f4dec0d1e641c804bf6c221e6f095a707
Alexander Medved <medved1991alexander@gmail.com>
2023-01-10 23:17:05 +0300

    add guitarShape controller

--- SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/GuitarShapeService.cs
+++ SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/GuitarShapeService.cs
@@ -5,19 +5,19 @@ using SoundPlay.BLL.ViewModels;
 namespace SoundPlay.WEB.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    public class GuitarShapeController : Controller
+    public class BrandController : Controller
     {
-        private readonly IItemGenericService<GuitarShapeViewModel> _guitarShapeService;
+        private readonly IItemGenericService<BrandViewModel> _brandService;
 
-        public GuitarShapeController(IItemGenericService<GuitarShapeViewModel> guitarShapeService)
+        public BrandController(IItemGenericService<BrandViewModel> brandService)
         {
-            _guitarShapeService = guitarShapeService;
+            _brandService = brandService;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var viewModels = await _guitarShapeService.GetViewModelsAsync();
+            var viewModels = await _brandService.GetViewModelsAsync();
             return View(viewModels);
         }
         
@@ -29,11 +29,11 @@ namespace SoundPlay.WEB.Areas.Admin.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(GuitarShapeViewModel obj)
+        public async Task<IActionResult> Create(BrandViewModel obj)
         {    
             if (ModelState.IsValid)
             {
-				await _guitarShapeService.CreateViewModelAsync(obj);
+				await _brandService.CreateViewModelAsync(obj);
 				return RedirectToAction("Index");
 			}
             return View(obj);
@@ -42,16 +42,16 @@ namespace SoundPlay.WEB.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> Edit(int id)
         {
-			var viewModel = await _guitarShapeService.GetViewModelByIdAsync(id);
+			var viewModel = await _brandService.GetViewModelByIdAsync(id);
             return View(viewModel);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Edit(GuitarShapeViewModel obj)
+        public async Task<IActionResult> Edit(BrandViewModel obj)
         {
             if (ModelState.IsValid)
             {
-				await _guitarShapeService.UpdateViewModelAsync(obj);
+				await _brandService.UpdateViewModelAsync(obj);
 				return RedirectToAction("Index");
 			}
             return View(obj);
@@ -61,14 +61,14 @@ namespace SoundPlay.WEB.Areas.Admin.Controllers
         [HttpGet]
         public async Task<IActionResult> Delete(int id)
         {
-            var viewModel = await _guitarShapeService.GetViewModelByIdAsync(id);
+            var viewModel = await _brandService.GetViewModelByIdAsync(id);
             return View(viewModel);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Delete(GuitarShapeViewModel obj)
+        public async Task<IActionResult> Delete(BrandViewModel obj)
         {
-			await _guitarShapeService.DeleteViewModelAsync(obj);
+			await _brandService.DeleteViewModelAsync(obj);
 			return RedirectToAction("Index");
 		}
     }

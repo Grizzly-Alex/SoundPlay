b3d88ac89b431b80cfaa832a325829f6dda66e4a
Alexander Medved <medved1991alexander@gmail.com>
2023-01-12 14:30:44 +0300

    add product model

--- SoundPlay/SoundPlay.BLL/Interfaces/IItemGenericService.cs
+++ SoundPlay/SoundPlay.BLL/Interfaces/IItemGenericService.cs
@@ -3,13 +3,9 @@
     public interface IItemGenericService<TViewModel> where TViewModel : class
     {
         public Task<IEnumerable<TViewModel>> GetViewModelsAsync();
-
         public Task<TViewModel> GetViewModelByIdAsync(int id);
-
         public Task<TViewModel> CreateViewModelAsync(TViewModel viewModel);
-
         public Task<TViewModel> UpdateViewModelAsync(TViewModel viewModel);
-
         public Task<TViewModel> DeleteViewModelAsync(TViewModel viewModel);
     }
 }
---
+++ SoundPlay/SoundPlay.DAL/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace SoundPlay.DAL.Models
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+        public string? ModelName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }  
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public int BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public int ColorId { get; set; }
+        public Color? Color { get; set; }
+    }
+}

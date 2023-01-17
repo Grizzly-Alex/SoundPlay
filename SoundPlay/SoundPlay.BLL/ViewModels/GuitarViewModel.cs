291b6dc576490f84b4dc3edbfdad0a721dc36e69
Alexander Medved <medved1991alexander@gmail.com>
2023-01-17 16:36:47 +0300

    add view guitar create

--- SoundPlay/SoundPlay.BLL/ViewModels/GuitarViewModel.cs
+++ SoundPlay/SoundPlay.BLL/ViewModels/GuitarViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using SoundPlay.DAL.Models;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace SoundPlay.BLL.ViewModels
 {
-    public sealed class GuitarViewModel:ProductViewModel
+    public sealed class GuitarViewModel : ProductViewModel
     {
-        [Required] public int FrestCount { get; set; }
-        [Required] public int StringsCount { get; set; }
+        [Required]
+		[DisplayName("Frest Count")]
+		public int FrestCount { get; set; }
+
+        [Required]
+        [DisplayName("Strings Count")] 
+        public int StringsCount { get; set; }
+
         public int? ShapeId { get; set; }
         public int? SoundboardId { get; set; }
         public int? NeckId { get; set; }
@@ -14,10 +21,20 @@ namespace SoundPlay.BLL.ViewModels
         public int? TremoloTypeId { get; set; }
         public int? PickupSetId { get; set; }
         public GuitarShape? Shape { get; set; }
+
+        [DisplayName ("Soundboard Material")] 
         public Material? Soundboard { get; set; }
+
+		[DisplayName("Neck Material")] 
         public Material? Neck { get; set; }
+
+		[DisplayName("Fretboard Material")]
         public Material? Fretboard { get; set; }
+
+		[DisplayName("Pickup Set")]
         public PickupSet? PickupSet { get; set; }
+
+		[DisplayName("Tremolo Type")]
         public TremoloType? TremoloType { get; set; }
     }
 }

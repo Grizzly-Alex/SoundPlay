24a8cae7dcde3d42a75a4cd4683bf0feebed1614
antonprakapniou <antonprokapnev@gmail.com>
2023-01-19 09:00:30 +0300

    Add basket models

---
+++ SoundPlay/SoundPlay.BLL/ViewModels/Customer/BasketPosition.cs
@@ -0,0 +1,24 @@
+﻿namespace SoundPlay.BLL.ViewModels.Customer
+{
+    public sealed class BasketPosition
+    {
+        private decimal _sum;
+        public Guid BasketPositionId { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string? ProductPictureUrl { get; set; }
+        public byte Count { get; set; }
+        public decimal Sum
+        {
+            get => _sum;
+            private set => _sum = ProductPrice*Count;
+        }
+
+        public BasketPosition()
+        {
+            BasketPositionId = Guid.NewGuid();
+        }
+
+    }
+}

8327c4c0f8a7950afb22df08e8d4a03c7eab0a21
Alexander Medved <medved1991alexander@gmail.com>
2023-01-22 22:56:13 +0300

    add java script for validation

--- SoundPlay/SoundPlay.WEB/wwwroot/js/site.js
+++ SoundPlay/SoundPlay.WEB/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const forms = document.querySelectorAll(".needs-validation");
+
+
+Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+    }, false);
+});

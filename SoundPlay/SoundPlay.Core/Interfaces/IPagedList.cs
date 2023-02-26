953be4ae90756ed5faacbde76e0e41903eb41059
Alexander Medved <medved1991alexander@gmail.com>
2023-02-26 23:12:48 +0300

    Add IPagedList

---
+++ SoundPlay/SoundPlay.Core/Interfaces/IPagedList.cs
@@ -0,0 +1,13 @@
+﻿namespace SoundPlay.Core.Interfaces;
+
+public interface IPagedList<TEntity> where TEntity : Entity
+{
+    int IndexFrom { get; }
+    int PageIndex { get; }  
+    int PageSize { get; }
+    int TotalCount { get; } 
+    int TotalPages { get; }
+    IList<TEntity> Items { get; }
+    bool HasPreviousPage { get; }
+    bool HasNextPage { get; }
+}
--- SoundPlay/SoundPlay.WEB/Constants.cs
+++ SoundPlay/SoundPlay.WEB/Constants.cs
@@ -4,4 +4,5 @@ public static class Constants
 {
     public const string BasketSession = "BasketSession";
     public const string GuitarsImages = @"/images/products/guitars/";
+    public const int ItemsPerPage = 12;
 }

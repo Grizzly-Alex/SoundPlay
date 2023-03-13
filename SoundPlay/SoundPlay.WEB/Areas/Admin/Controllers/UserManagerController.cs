394519489494bdefc6ca33432fcd47bc1083a9b7
Alexander Medved <medved1991alexander@gmail.com>
2023-03-13 18:01:58 +0300

    mend

--- SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/UserManagerController.cs
+++ SoundPlay/SoundPlay.WEB/Areas/Admin/Controllers/UserManagerController.cs
@@ -1,35 +1,32 @@
 ﻿namespace SoundPlay.Web.Areas.Admin.Controllers;
 
 [Area("Admin")]
-public class IdentityManagerController : Controller
+public class UserManagerController : Controller
 {
     private readonly UserManager<AppUser> _userManager;
     private readonly IUserStore<AppUser> _userStore;
-    //private readonly IUserEmailStore<AppUser> _emailStore;
-    private readonly ILogger<IdentityManagerController> _logger;
+    private readonly ILogger<UserManagerController> _logger;
     private readonly IMapper _mapper;
     private readonly RoleManager<IdentityRole> _roleManager;
 
-    public IdentityManagerController(
+    public UserManagerController(
         UserManager<AppUser> userManager,
         IUserStore<AppUser> userStore,
-        //IUserEmailStore<AppUser> emailStore,
-        ILogger<IdentityManagerController> logger,
+        ILogger<UserManagerController> logger,
         IMapper mapper,
         RoleManager<IdentityRole> roleManager)
     {
         _userManager = userManager;
         _userStore = userStore;
-        //_emailStore = emailStore; 
         _logger = logger;
         _mapper = mapper;
         _roleManager = roleManager;
     }
 
     [HttpGet]
-    public async Task<IActionResult> Index(Roles role)
+    public async Task<IActionResult> Index(string role)
     {
-        ViewData["role"] = role;
+        TempData["role"] = role;
         var customers = await _userManager.GetUsersInRoleAsync(role.ToString());
         return View(customers);
     }
@@ -40,7 +37,8 @@ public class IdentityManagerController : Controller
         var user = new UserViewModel
         {
             RoleList = _roleManager.Roles
-            .Select(x => x.Name)
+            .Where(i =>  i.Name != Roles.Customer.ToString())
+            .Select(i => i.Name)
             .Select(i => new SelectListItem { Text = i, Value = i })
         };
         return View(user);
@@ -59,8 +57,30 @@ public class IdentityManagerController : Controller
             {
                 _logger.LogInformation("User created a new account with password.");
                 await _userManager.AddToRoleAsync(user, userView.Role);
+
+                return RedirectToAction(nameof(Index), new {role = userView.Role});
             }
         }
-        return RedirectToAction(nameof(Index), userView.Role);
+        return RedirectToAction(nameof(Create));
+    }
+
+    public async Task<IActionResult> Delete(string userId)
+    {     
+        var user = await _userManager.FindByIdAsync(userId);
+
+        if (user is not null) 
+        {
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded)
+            {
+                _logger.LogInformation("User deleted.");
+            }
+        }
+        else
+        {
+            _logger.LogInformation("User not found.");
+        }
+        return RedirectToAction(nameof(Index), new { role = TempData["role"] });
     }
 }
